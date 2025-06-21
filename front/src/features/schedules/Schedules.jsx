32b9276ffc0f655d5281ec05c8b5d91f8de4facb
Ignatius K <ignatiuskisekka@gmail.com>
2025-06-21 15:15:34 +0300

    add basic layout

---
+++ front/src/features/schedules/Schedules.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Schedules = () => {
+	return (
+		<>
+			<div>Schedules</div>
+			<Outlet />
+		</>
+	);
+};
+
+export default Schedules;
--- front/src/helpers/axios_helper.js
+++ front/src/helpers/axios_helper.js
@@ -1,77 +1,70 @@
 import axios from "axios";
 
-
-console.log(import.meta.env)
 export const baseUrl = import.meta.env.VITE_BASE_URL;
-console.log(baseUrl)
 
 export const AxiosPublic = axios.create({
-  baseUrl,
-  headers: {
-    'Content-Type': 'application/json',
-  }
-})
+	baseUrl,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
 
-
-export const AxiosPrivate  = axios.create({
-  baseUrl,
-  headers: {
-    'Content-Type': 'application/json',
-  }
-})
-
+export const AxiosPrivate = axios.create({
+	baseUrl,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
 
 export const AxiosConfiguration = {
-  initialize: () => {
 
-    const transformResponse = (response) => {
-      //TODO: Transform resposnse
-      return response
-    }
+    /**
+     * 
+     * @param {import("@reduxjs/toolkit").EnhancedStore} store 
+     */
+	initialize: (store) => {
+		const transformResponse = (response) => {
+			//TODO: Transform resposnse
+			return response;
+		};
 
+		const transformResponseError = (error) => {
+			//TODO: Transform resposnse
+			return Promise.reject(error);
+		};
 
-    const transformResponseError = (error) => {
-      //TODO: Transform resposnse
-      return Promise.reject(error)
-    }
+		AxiosPrivate.interceptors.request.use(
+			(config) => {
+				//FIXME: To handle the authentication
+				const token = localStorage.getItem("token");
+				if (token) {
+					config.headers.Authorization = `Bearer ${token}`;
+				}
+				return config;
+			},
+			(error) => {
+				return Promise.reject(error);
+			}
+		);
 
-    AxiosPrivate.interceptors.request.use(
-      (config) => {
-        //FIXME: To handle the authentication
-        const token = localStorage.getItem('token');
-        if (token) {
-          config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-      },
-      (error) => {
-        return Promise.reject(error);
-      }
-    );
+		AxiosPrivate.interceptors.response.use(
+			transformResponse,
+			transformResponseError
+		);
 
-  AxiosPrivate.interceptors.response.use(transformResponse, transformResponseError)
+		AxiosPublic.interceptors.request.use(
+			(config) => {
+				//TODO: Transform request
+				return config;
+			},
+			(error) => {
+				return Promise.reject(error);
+			}
+		);
 
-  AxiosPublic.interceptors.request.use(
-    (config) => {
-      //TODO: Transform request
-      return config
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  )
-
-    AxiosPublic.interceptors.response.use(transformResponse, transformResponseError)
-}
-}
-
-
-
-
-
-
-
-
-
-
-
+		AxiosPublic.interceptors.response.use(
+			transformResponse,
+			transformResponseError
+		);
+	},
+};
